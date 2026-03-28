25d2c737621cf8ee4c4cf28047ceb1d4fd55dd80
155TuT <zhiyiwang155TuT@gmail.com>
2026-03-28 14:21:11 +0800

    refactor: structure of file

--- teaching/Week1/exercise/max_in_Xn/max_in_Xn.asm
+++ teaching/Week1/exercise/max_in_Xn/max_in_Xn.asm
@@ -2,29 +2,29 @@
 include irvine32.inc
 
 .data	;数据段
-    DAT WORD 1, -2, 3, -4, 155, -6, 7
-    MAX WORD ?
+  DAT WORD 1, -2, 3, -4, 155, -6, 7
+  MAX WORD ?
 
 .code	;代码段
 main PROC
-    mov ecx, LENGTHOF DAT - 1 ; loop symbol
-    mov esi, 0                ; index of DAT
-    mov ax, DAT[esi]          ; init
+  mov ecx, LENGTHOF DAT - 1 ; loop symbol
+  mov esi, 0                ; index of DAT
+  mov ax, DAT[esi]          ; init
 
 AGAIN:
-    add esi, 2                ; TYPE DAT = 2
-    cmp ax, DAT[esi]          ; compare ax with DAT[esi]
-    jge NEXT                  ; jump NEXT if ax greater than DAT[esi](or equal)
-    mov ax, DAT[esi]
+  add esi, 2                ; TYPE DAT = 2
+  cmp ax, DAT[esi]          ; compare ax with DAT[esi]
+  jge NEXT                  ; jump NEXT if ax greater than DAT[esi](or equal)
+  mov ax, DAT[esi]
 
 NEXT:
-    dec ecx                   ; (loop symbol)ecx--
-    jnz AGAIN                 ; jump AGAIN if ecx not zero(loop)
+  dec ecx                   ; (loop symbol)ecx--
+  jnz AGAIN                 ; jump AGAIN if ecx not zero(loop)
 
-    mov MAX, ax
-    movsx eax, MAX            ; 16bit to 32bit
-    call WriteInt             ; output
+  mov MAX, ax
+  movsx eax, MAX            ; 16bit to 32bit
+  call WriteInt             ; output
 
-    exit
+  exit
 main ENDP
 END main
---
+++ teaching/fzuasm/README.md
@@ -0,0 +1,5 @@
+# 快捷指令
+
+```cmd
+%FZUASM_ROOT%\make32.bat 文件名
+```
